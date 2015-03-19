94ce9eb1563927a070bcd86677a4f2947b302ae3
roc-sqa <X2y73cn101>
2015-03-18 22:38:10 -0400

    Browser changes

---
+++ WebTestProto/Framework/Browser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace WebTestProto.Framework
+{
+    public static class Browser
+    {
+        static IWebDriver webDriver;
+
+        public static ISearchContext Driver { get { return webDriver; } }
+
+        public static void SetDriver()
+        {
+            webDriver = new FirefoxDriver();
+        }
+
+        public static void Goto(string url)
+        {
+            webDriver.Url = url;
+        }
+
+        public static void Close()
+        {
+            webDriver.Close();
+        }
+
+        public static void Quit()
+        {
+            webDriver.Quit();
+        }
+    }
+}
